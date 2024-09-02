c2f6e04ace7c37412128f932f746e578e864bea1
wangzhongshu <wzscq@126.com>
2024-09-02 11:41:54 +0800

    commit changes

--- build/build_dockerimage.sh
+++ build/build_dockerimage.sh
@@ -1,3 +1,3 @@
 docker image rm wangzhsh/gwprjservice:0.1.0
-docker build . -t wangzhsh/gwprjservice:0.1.0
-docker push wangzhsh/gwprjservice:0.1.0
+docker build . -t registry.cn-hangzhou.aliyuncs.com/wangzhsh/gwprjservice:0.1.0
+docker push registry.cn-hangzhou.aliyuncs.com/wangzhsh/gwprjservice:0.1.0
